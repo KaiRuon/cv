617084ffa8f49b489c897dd2daedcfa49555c0a2
KaiRuon <65549530+KaiRuon@users.noreply.github.com>
2020-07-13 22:20:15 +0900

    Update Footer + Contact

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
 $(document).ready(function () {
   $(window).scroll(function () {
-    if (this.scrollY > 20) $(".menu").addClass("sticky");
-    else $(".menu").removeClass("sticky");
+    if (this.scrollY > 50) {
+      $(".menu").addClass("sticky");
+      $(".gotop").fadeIn();
+    } else {
+      $(".menu").removeClass("sticky");
+      $(".gotop").fadeOut();
+    }
+  });
+
+  $(".gotop").click(function () {
+    scroll(0, 0);
   });
 
   $(".menu-toggler").click(function () {
